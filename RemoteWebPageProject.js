8eddd61db4ec81023b9eb04d8f2e667b5ebc830b
johnjbarton <johnjbarton@johnjbarton.com>
2012-09-07 16:40:48 -0700

    Better OOP

--- RemoteWebPageProject.js
+++ RemoteWebPageProject.js
@@ -4,9 +4,8 @@
 // A traceur WebPageProject that delegates XHR to chrome extension background and
 // extracts scripts via chrome.inspectedWindow.eval
 
-function RemoteWebPageProject(remoteURL, remoteScripts) {
+function RemoteWebPageProject(remoteURL) {
   WebPageProject.call(this, remoteURL);
-  this.remoteScripts = remoteScripts;
   RemoteWebPageProject.currentProject = this;
   console.log("project created for "+remoteURL);
 }
@@ -25,12 +24,12 @@ RemoteWebPageProject.backgroundPort = new ChannelPlate.DevtoolsTalker(RemoteWebP
 
 RemoteWebPageProject.prototype = Object.create(WebPageProject.prototype);
 
-RemoteWebPageProject.prototype.runWebPage = function() {
+RemoteWebPageProject.prototype.run = function() {
   this.addFilesFromScriptElements(this.remoteScripts);
   this.runScriptsIfNonePending_();
 }
 
-RemoteWebPageProject.prototype._asyncLoad = function(url, fncOfContent) {
+RemoteWebPageProject.prototype.asyncLoad_ = function(url, fncOfContent) {
   this.numPending++;
   // mihaip@chromium.org on https://groups.google.com/a/chromium.org/d/msg/chromium-extensions/-/U33r217_Px8J
   // The whitelisting for cross-origin XHRs only happens when running in an extension process. 
@@ -67,7 +66,7 @@ RemoteWebPageProject.prototype.putFiles = function(files) {
     var source = file.generatedSource + "\n//@ sourceURL=" + file.name;
     return {content: source};
   });
-  putPageScripts(scripts, function(result) {
+  this.putPageScripts(scripts, function(result) {
     console.log(" and the result is... ", result);
   });
 };
@@ -75,11 +74,11 @@ RemoteWebPageProject.prototype.putFiles = function(files) {
 //----------------------------------------------------------------------------------------------------------
 // chrome.devtools.inspectedWindow.eval() based script extractor
 
-function evalStringify(fnc, args) {
+RemoteWebPageProject.prototype.evalStringify = function(fnc, args) {
   return '(' +fnc.toString() + '(' + JSON.stringify(args) + ')'+ ');'
 }
 
-function getPageScripts(callback) {
+RemoteWebPageProject.prototype.getPageScripts = function(callback) {
   // We can't access the page directly and 
   // script elements with type="application/traceur" are not loaded as devtools resources.
 
@@ -96,15 +95,21 @@ function getPageScripts(callback) {
     }
     return scripts;
   }
-  chrome.devtools.inspectedWindow.eval(evalStringify(getScripts, []), callback);
-}
 
-function putPageScripts(scripts, callback) {
-  function putScripts(scripts) {
+  function onScripts(remoteScripts) { // runs here
+    this.remoteScripts = remoteScripts;
+    callback();
+  }
+
+  chrome.devtools.inspectedWindow.eval(this.evalStringify(getScripts, []), onScripts.bind(this));
+};
+
+RemoteWebPageProject.prototype.putPageScripts = function(scripts, callback) {
+  function putScripts(scripts) { // runs in web page
     scripts.forEach(function(script) {
       var content = script.content;
       eval(content);
     });
   }
-  return chrome.devtools.inspectedWindow.eval(evalStringify(putScripts, scripts), callback);
+  return chrome.devtools.inspectedWindow.eval(this.evalStringify(putScripts, scripts), callback);
 }
