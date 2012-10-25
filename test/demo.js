1b183003942c43c9b7692d53e79cff43c9f2dbf1
johnjbarton <johnjbarton@johnjbarton.com>
2012-10-25 16:56:52 -0700

    replace tab by space to make offsets less confusing

--- test/demo.js
+++ test/demo.js
@@ -5,41 +5,41 @@ var obj = {
 };
 
 var condition = {
-	value: 1
+  value: 1
 };
 
 var oldValue;
 
 function onClick() {
-	foo();
-	obj.prop++;
-	bar();
-	// lots more code....
-	updateButton();
+  foo();
+  obj.prop++;
+  bar();
+  // lots more code....
+  updateButton();
 }
 
 function updateButton() {
-	var message = "I've been clicked " + obj.prop + " times";
-	document.querySelector("#myButton").innerHTML = message;
-	console.log(message);
+  var message = "I've been clicked " + obj.prop + " times";
+  document.querySelector("#myButton").innerHTML = message;
+  console.log(message);
 }
 
 function foo() {
-	condition.value = oldValue;
+  condition.value = oldValue;
 }
 
 function bar() {
-	if (!condition.value) {
-		obj.prop = 0;
-	}
+  if (!condition.value) {
+    obj.prop = 0;
+  }
 }
 
 function onLoad() {
-	var button = document.createElement('button');
-	button.innerHTML = "Try Me!";
-	button.addEventListener('click', onClick);
-	button.id = "myButton";
-    document.body.appendChild(button);	
+  var button = document.createElement('button');
+  button.innerHTML = "Try Me!";
+  button.addEventListener('click', onClick);
+  button.id = "myButton";
+  document.body.appendChild(button);  
 }
 
 window.addEventListener('load',onLoad);
